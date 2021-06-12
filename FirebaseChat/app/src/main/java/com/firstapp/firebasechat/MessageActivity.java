0f30d729542e3ff31e8736126973e5f6b73224f3
abstractprototype <cheayuenchen@gmail.com>
2021-06-12 01:41:35 -0700

    Attempting to send image

--- FirebaseChat/app/src/main/java/com/firstapp/firebasechat/MessageActivity.java
+++ FirebaseChat/app/src/main/java/com/firstapp/firebasechat/MessageActivity.java
@@ -64,8 +64,6 @@ public class MessageActivity extends AppCompatActivity {
     TextView username;
     ImageView imageView;
 
-    RecyclerView recyclerViewy, mMedia;
-    RecyclerView.Adapter mMediaAdapter;
     EditText msg_editText;
     ImageButton sendBtn;
     ImageButton imageBtn;
@@ -91,11 +89,13 @@ public class MessageActivity extends AppCompatActivity {
 
     MessageAdapter messageAdapter;
     MediaAdapter mediaAdapter;
-    private RecyclerView.LayoutManager mMediaLayoutManager;
     List<Chat> mChat;
     ArrayList<String> mediaUriList; //For sending images, adds uri to this list
 
     RecyclerView recyclerView;
+    RecyclerView recyclerViewy, mMedia;
+    private RecyclerView.LayoutManager mMediaLayoutManager;
+    RecyclerView.Adapter mMediaAdapter;
     String userid;
 
     ValueEventListener seenListener;
@@ -104,6 +104,8 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        //initializeMedia();
 
         //Widgets
         imageView = findViewById(R.id.imageView_profilepic);
@@ -168,59 +170,84 @@ public class MessageActivity extends AppCompatActivity {
         });
 
         //When sending image button is pressed...
-        imageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CharSequence options[] = new CharSequence[]
-                {
-                    "Images",
-                    "PDF Files",
-                    "MS Word Files"
-                };
-                //Pop up message prompt asking to pick type of file to send
-                AlertDialog.Builder builder = new AlertDialog.Builder(MessageActivity.this);
-                builder.setTitle("Choose a File");
-
-                builder.setItems(options, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        if(i == 0){
-                            checker = "image";
-                            Intent intent = new Intent();
-                            intent.setAction(Intent.ACTION_GET_CONTENT);
-                            intent.setType("image/*");
-                            startActivityForResult(intent.createChooser(intent, "Select Image"), 438);
-                        }
-                        if(i == 1){
-                            checker = "pdf";
-                        }
-                        if(i == 2){
-                            checker = "docx";
-                        }
-                    }
-                });
-                builder.show();
-            }
-        });
-
 //        imageBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
-//                openGallery();
+//                CharSequence options[] = new CharSequence[]
+//                {
+//                    "Images",
+//                    "PDF Files",
+//                    "MS Word Files"
+//                };
+//                //Pop up message prompt asking to pick type of file to send
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MessageActivity.this);
+//                builder.setTitle("Choose a File");
+//
+//                builder.setItems(options, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int i) {
+//                        if(i == 0){
+//                            checker = "image";
+//                            Intent intent = new Intent();
+//                            intent.setAction(Intent.ACTION_GET_CONTENT);
+//                            intent.setType("image/*");
+//                            startActivityForResult(intent.createChooser(intent, "Select Image"), 438);
+//                        }
+//                        if(i == 1){
+//                            checker = "pdf";
+//                        }
+//                        if(i == 2){
+//                            checker = "docx";
+//                        }
+//                    }
+//                });
+//                builder.show();
 //            }
 //        });
+
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
         SeenMessage(userid);
 
     }
 
-//    //Opens user's gallery
-//    private void openGallery(){
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(Intent.createChooser(intent, "Select Picture"),IMAGE_REQUEST);
+//    private void initializeMedia(){
+//        mediaUriList = new ArrayList<>();
+//        //mMedia = findViewById(R.id.mediaList); //The recycler view that opens up when you are placing and choosing your images to send
+//        mMediaAdapter = new MediaAdapter(getApplicationContext(), mediaUriList);
+//        mMedia.setAdapter(mMediaAdapter);
 //    }
+
+    //Opens user's gallery
+    private void openGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture(s)"), IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == IMAGE_REQUEST){
+                if(data.getClipData() == null){
+                    mediaUriList.add(data.getData().toString()); //Adds 1 picture to our imageUri list in String
+                }else{
+                    for(int i = 0; i < data.getClipData().getItemCount(); i++){//Adds multiple pictures to our imageUri in String
+                        mediaUriList.add(data.getClipData().getItemAt(i).getUri().toString());
+                    }
+                }
+                mMediaAdapter.notifyDataSetChanged();
+            }
+        }
+    }
 
 
 //    //For sending image
@@ -243,94 +270,94 @@ public class MessageActivity extends AppCompatActivity {
 //        }
 //    }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 438 && resultCode == RESULT_OK && data != null && data.getData() != null){
-
-            loadingBar.setTitle("Sending File");
-            loadingBar.setMessage("Please wait, your file is sending...");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-
-            //Storing file from phone gallery into this fileUri
-            fileUri = data.getData();
-
-            if(!checker.equals("image")){
-                Toast.makeText(this, "Please choose a proper image", Toast.LENGTH_SHORT).show();
-            }
-            else if(checker.equals("image")){
-                storageReference = FirebaseStorage.getInstance().getReference().child("images");
-
-                String messageSenderRef = "Messages/" + messageSenderId + "/" + messageReceiverId;
-                String messageReceiverRef = "Messages/" + messageReceiverId + "/" + messageSenderId;
-
-                DatabaseReference userMessageKeyRef = reference.child("Messages")
-                        .child(messageSenderId).child(messageReceiverId).push();
-
-                final String messagePushID = userMessageKeyRef.getKey();
-
-                //Adds the extension of the image into folder
-                StorageReference filePath = storageReference.child(messagePushID + "." + "jpg");
-
-                //Puts image file in this path
-                uploadTask = filePath.putFile(fileUri);
-
-                uploadTask.continueWithTask(new Continuation() {
-                    @Override
-                    public Object then(@NonNull Task task) throws Exception {
-
-                        if(!task.isSuccessful()){
-                            throw task.getException();
-                        }
-                        return filePath.getDownloadUrl();
-                    }
-                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Uri> task) {
-                        if(task.isSuccessful()){
-
-                            Uri downloadUrl = task.getResult();
-                            myUrl = downloadUrl.toString();
-
-                            HashMap<String, Object> messagePictureBody = new HashMap<>();
-                            messagePictureBody.put("sender", messageSenderId);
-                            messagePictureBody.put("receiver", messageReceiverId);
-                            messagePictureBody.put("messageID", messagePushID);
-                            messagePictureBody.put("message", myUrl);
-                            messagePictureBody.put("tyoe", checker);
-                            messagePictureBody.put("name", fileUri.getLastPathSegment());
-                            messagePictureBody.put("isseen",false);
-//                            hashMap.put("time", currentTime);
-//                            hashMap.put("date", currentDate);
-
-                            HashMap<String, Object> messageBodyDetails = new HashMap<>();
-                            messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageBodyDetails);
-                            messageBodyDetails.put(messageReceiverRef + "/" + messagePushID, messageBodyDetails);
-
-//                            reference.updateChildren(messageBodyDetails).addOnCompleteListener((task1) -> {
-//                                if (task1.isSuccessful()){
-//                                    loadingBar.dismiss();
-//                                    Toast.makeText(MessageActivity.this, "Message sent successfully", Toast.LENGTH_SHORT).show();
-//                                }
-//                                else{
-//                                    loadingBar.dismiss();
-//                                    Toast.makeText(MessageActivity.this, "Error", Toast.LENGTH_SHORT).show();
-//                                }
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        super.onActivityResult(requestCode, resultCode, data);
 //
-//                            });
-
-                        }
-                    }
-                });
-
-            }
-            else{
-                loadingBar.dismiss();
-                Toast.makeText(MessageActivity.this, "Nothing selected, try again", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+//        if(requestCode == 438 && resultCode == RESULT_OK && data != null && data.getData() != null){
+//
+//            loadingBar.setTitle("Sending File");
+//            loadingBar.setMessage("Please wait, your file is sending...");
+//            loadingBar.setCanceledOnTouchOutside(false);
+//            loadingBar.show();
+//
+//            //Storing file from phone gallery into this fileUri
+//            fileUri = data.getData();
+//
+//            if(!checker.equals("image")){
+//                Toast.makeText(this, "Please choose a proper image", Toast.LENGTH_SHORT).show();
+//            }
+//            else if(checker.equals("image")){
+//                storageReference = FirebaseStorage.getInstance().getReference().child("images");
+//
+//                String messageSenderRef = "Messages/" + messageSenderId + "/" + messageReceiverId;
+//                String messageReceiverRef = "Messages/" + messageReceiverId + "/" + messageSenderId;
+//
+//                DatabaseReference userMessageKeyRef = reference.child("Messages")
+//                        .child(messageSenderId).child(messageReceiverId).push();
+//
+//                final String messagePushID = userMessageKeyRef.getKey();
+//
+//                //Adds the extension of the image into folder
+//                StorageReference filePath = storageReference.child(messagePushID + "." + "jpg");
+//
+//                //Puts image file in this path
+//                uploadTask = filePath.putFile(fileUri);
+//
+//                uploadTask.continueWithTask(new Continuation() {
+//                    @Override
+//                    public Object then(@NonNull Task task) throws Exception {
+//
+//                        if(!task.isSuccessful()){
+//                            throw task.getException();
+//                        }
+//                        return filePath.getDownloadUrl();
+//                    }
+//                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Uri> task) {
+//                        if(task.isSuccessful()){
+//
+//                            Uri downloadUrl = task.getResult();
+//                            myUrl = downloadUrl.toString();
+//
+//                            HashMap<String, Object> messagePictureBody = new HashMap<>();
+//                            messagePictureBody.put("sender", messageSenderId);
+//                            messagePictureBody.put("receiver", messageReceiverId);
+//                            messagePictureBody.put("messageID", messagePushID);
+//                            messagePictureBody.put("message", myUrl);
+//                            messagePictureBody.put("tyoe", checker);
+//                            messagePictureBody.put("name", fileUri.getLastPathSegment());
+//                            messagePictureBody.put("isseen",false);
+////                            hashMap.put("time", currentTime);
+////                            hashMap.put("date", currentDate);
+//
+//                            HashMap<String, Object> messageBodyDetails = new HashMap<>();
+//                            messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageBodyDetails);
+//                            messageBodyDetails.put(messageReceiverRef + "/" + messagePushID, messageBodyDetails);
+//
+////                            reference.updateChildren(messageBodyDetails).addOnCompleteListener((task1) -> {
+////                                if (task1.isSuccessful()){
+////                                    loadingBar.dismiss();
+////                                    Toast.makeText(MessageActivity.this, "Message sent successfully", Toast.LENGTH_SHORT).show();
+////                                }
+////                                else{
+////                                    loadingBar.dismiss();
+////                                    Toast.makeText(MessageActivity.this, "Error", Toast.LENGTH_SHORT).show();
+////                                }
+////
+////                            });
+//
+//                        }
+//                    }
+//                });
+//
+//            }
+//            else{
+//                loadingBar.dismiss();
+//                Toast.makeText(MessageActivity.this, "Nothing selected, try again", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
 //    private String getFileExtension(Uri uri){
 //        ContentResolver contentResolver = getContentResolver();
@@ -371,15 +398,16 @@ public class MessageActivity extends AppCompatActivity {
     private void sendMessage(String sender, String receiver, String message){
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
+        //Adds message info to database
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("sender", sender);
         hashMap.put("receiver", receiver);
         hashMap.put("message", message);
         hashMap.put("isseen",false);
-
         reference.child("Chats").push().setValue(hashMap);
 
-        //Adding User to chat fragment: Recent chats with contacts
+
+        //After chatting with user, adds User to chat fragment: Recent chats with contacts
         final DatabaseReference chatRef = FirebaseDatabase.getInstance()
                 .getReference("ChatList")
                 .child(fuser.getUid())
@@ -399,7 +427,18 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void sendImage(){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        if(!mediaUriList.isEmpty()){
+            for(String mediaUri : mediaUriList){
+                String mediaId = reference.child("media").push().getKey();
+                //final StorageReference filePath = FirebaseStorage.getInstance().getReference().child("images").child()
+            }
+        }
     }
 
 //    private void sendImage(String sender, String receiver, String message){
@@ -462,6 +501,8 @@ public class MessageActivity extends AppCompatActivity {
         });
 
     }
+
+
 
     //Checks online or offline status of a user
     private void CheckStatus(String status){
